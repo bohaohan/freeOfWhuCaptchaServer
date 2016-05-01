02d0f5bd61479750afeca2483699d4490f34781c
bohaohan <54bohaohan@gmail.com>
2016-05-02 00:36:56 +0800

    add new method

--- eduLogin/captcha/captcha_new.py
+++ eduLogin/captcha/captcha_new.py
@@ -9,8 +9,9 @@ class CaptchaNew(object):
        'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
        'x', 'y', 'z']
+    model = self.get_model()
     def __init__(self):
-        self.model = self.get_model()
+        pass
 
     def get_model(self):
         classes = 36
